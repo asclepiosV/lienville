6cef844160673cfb809524a276cf594acb08830c
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-29 22:53:38 +0200

    add transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-import time
 
 voiture = 55 #CO2 en g / passager/km
 bus = 68
@@ -27,7 +26,7 @@ def rechercheDepart(depart):
                 master[-1][3] = calculCarbon(int(master[-1][3]), master[-1][2])
 
 
-def rechercheVille2(ville, tab, history, carbon):
+def rechercheVille2(ville, tab, history, carbon,transport):
     for i in range(0, len(liste)):
         listTmp = list(liste[i])
         historyTmp = list(history)
@@ -37,20 +36,19 @@ def rechercheVille2(ville, tab, history, carbon):
                 listTmp[1] = listTmp[0]
                 listTmp[0] = ville
             listTmp.append(historyTmp)
+            listTmp.append([transport,listTmp[2]])
             tab.append(listTmp)
             tab[-1][3] = calculCarbon(int(tab[-1][3]), tab[-1][2]) + int(carbon)
 
 
-def rechercheVille3(ville, tab, history, carbon):
+def rechercheVille3(ville, tab, history, carbon, transport):
     for i in range(0, len(liste)):
         bool = True
         listTmp = list(liste[i])
         historyTmp = list(history)
         historyTmp.append(ville)
+        transportTmp = list(transport)
         if ville in listTmp:
-            """print("Histo " + str(historyTmp))
-            print(ville)
-            print("listTmp" + str(listTmp))"""
             for j in range(len(historyTmp) - 1):
                 if historyTmp[j] in listTmp:
                     bool = False
@@ -61,6 +59,8 @@ def rechercheVille3(ville, tab, history, carbon):
                 listTmp[1] = str(listTmp[0])
                 listTmp[0] = str(ville)
             listTmp.append(historyTmp)
+            transportTmp.append(listTmp[2])
+            listTmp.append(transportTmp)
             tab.append(listTmp)
             tab[-1][3] = calculCarbon(int(tab[-1][3]), tab[-1][2]) + int(carbon)
 
@@ -87,14 +87,15 @@ def main():
     rechercheDepart(depart)
     tmp = []
     for i in range(len(master)):
-        rechercheVille2(master[i][1], tmp, master[i][4], master[i][3])
+        rechercheVille2(master[i][1], tmp, master[i][4], master[i][3], master[i][2])
     masterEnd = [list(master), list(tmp)]
     fin = 0
     while True:
         tmp = []
         for i in range(len(masterEnd[len(masterEnd) - 1])):
             if masterEnd[len(masterEnd)-1][i][1] != end:
-                rechercheVille3(masterEnd[len(masterEnd)-1][i][1], tmp, masterEnd[len(masterEnd)-1][i][4], masterEnd[len(masterEnd)-1][i][3])
+                rechercheVille3(masterEnd[len(masterEnd)-1][i][1], tmp, masterEnd[len(masterEnd)-1][i][4], masterEnd[len(masterEnd)-1][i][3]
+                                ,masterEnd[-1][i][5])
             else:
                 resultat.append(masterEnd[len(masterEnd)-1][i])
         masterEnd.append(tmp)
@@ -106,14 +107,18 @@ openCsv()
 
 masterEnd = main()
 
-temp = 1000000000000
+temp = resultat[0][3]
 sauv = []
+transportfinal = []
 
 for line in resultat:
     if line[3] < temp:
         temp = line[3]
         sauv = list(line[4])
+        transportfinal = list(line[5])
         sauv.append(end)
 
-print("\nLe trajet passe par le villes suviantes : " + str(sauv) + " avec une consomation de " + str(temp) + "g de CO2")
+print("\nLe trajet passe par le villes suviantes : " + str(sauv) + " avec une consomation de "
+      + str(temp) + "g de CO2 en utilisant" + str(transportfinal))
+
 
